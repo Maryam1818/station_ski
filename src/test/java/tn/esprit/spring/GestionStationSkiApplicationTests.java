70e86b65c33f319048507775f99214dd35ce1b83
amin080 <amine.ouhichi@esprit.tn>
2023-10-31 15:55:38 +0100

    modif007

---
+++ src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
@@ -0,0 +1,16 @@
+package tn.esprit.spring;
+
+import org.junit.jupiter.api.Test;
+
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class GestionStationSkiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
