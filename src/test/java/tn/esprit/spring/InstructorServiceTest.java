763f2a3bc486094717ac85472c1c3c3aa77a6ca3
Oumeyma-Nefzi <92586081+Oumeyma-Nefzi@users.noreply.github.com>
2023-11-11 20:47:37 +0100

    dockerFile

---
+++ src/test/java/tn/esprit/spring/InstructorServiceTest.java
@@ -0,0 +1,8 @@
+package tn.esprit.spring;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class InstructorServiceTest {
+}
